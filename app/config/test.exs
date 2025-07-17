db2a976a3b57d47f6d0d5f94e424d0cea5879096
Simon Oulevay <simon.oulevay@gmail.com>
2025-07-17 22:18:58 +0200

    Configure fake openid connect credentials for testing

--- app/config/test.exs
+++ app/config/test.exs
@@ -42,3 +42,10 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+# Configure fake OpenID Connect provider for testing.
+config :ueberauth_oidcc, :providers,
+  switch_edu_id: [
+    client_id: "id",
+    client_secret: "secret"
+  ]
