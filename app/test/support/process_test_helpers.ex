333cd5ec6e279c0c99b5576294327a8f771d68ca
Simon Oulevay <simon.oulevay@gmail.com>
2025-07-22 21:15:02 +0200

    Fix process test helpers test

--- app/test/support/process_test_helpers.ex
+++ app/test/support/process_test_helpers.ex
@@ -60,7 +60,7 @@ defmodule ArchiDep.Support.ProcessTestHelpers do
       ...>   Agent.update(agent, fn data -> ["foo" | data] end)
       ...> end)
       iex> {usec, :ok} = :timer.tc(fn -> wait_for!(fn -> Agent.get(agent, fn state -> Enum.member?(state, "foo") end) end, "oops") end)
-      iex> assert_in_delta usec, 15_000, 5_000
+      iex> assert_in_delta usec, 15_000, 15_000
       iex> wait_for!(fn -> Agent.get(agent, fn state -> Enum.member?(state, "bar") end) end, "bar not found", 10)
       ** (RuntimeError) Condition never fulfilled: bar not found
   """
