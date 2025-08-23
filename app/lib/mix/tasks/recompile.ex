a9e5599458d09c1a85e8dd79ac0c739703d1113c
Simon Oulevay <simon.oulevay@gmail.com>
2025-08-23 18:06:40 +0200

    Fix lint issue

--- app/lib/mix/tasks/recompile.ex
+++ app/lib/mix/tasks/recompile.ex
@@ -1,19 +1,25 @@
 defmodule Mix.Tasks.Recompile do
+  @shortdoc "Force recompilation"
+
   @moduledoc """
   A custom Mix task to force recompilation of the project.
   """
 
   use Mix.Task
 
-  @shortdoc "Force recompilation"
-  def run(_) do
+  @spec run(term()) :: :ok
+  def run(_anything) do
     recompile_module = Path.join([File.cwd!(), "lib", "recompile.ex"])
 
     File.write!(recompile_module, """
     defmodule Recompile do
       @moduledoc false
+
+      require Logger
+
       @recompile "#{DateTime.utc_now()}"
-      IO.puts("Recompile module loaded at \#{@recompile}")
+
+      Logger.info("Recompile module loaded at \#{@recompile}")
     end
     """)
   end
