b6bd88e9545b49174e58269e99f9efe345a55cb1
Simon Oulevay <simon.oulevay@gmail.com>
2025-08-30 20:23:07 +0200

    Test load average result

--- app/test/archidep/servers/server_tracking/server_manager_state_test.exs
+++ app/test/archidep/servers/server_tracking/server_manager_state_test.exs
@@ -1786,7 +1786,7 @@ defmodule ArchiDep.Servers.ServerTracking.ServerManagerStateTest do
               ), %ServerManagerState{result | version: result.version + 1}}
   end
 
-  test "the connection process is stopped if the application user does not have sudo access", %{
+  test "fact gathering is not triggered if the application user does not have sudo access", %{
     handle_task_result: handle_task_result
   } do
     server = build_active_server(set_up_at: true, ssh_port: true)
@@ -1899,7 +1899,7 @@ defmodule ArchiDep.Servers.ServerTracking.ServerManagerStateTest do
               ), %ServerManagerState{result | version: result.version + 1}}
   end
 
-  test "the connection process is stopped if sudo access cannot be checked", %{
+  test "fact gathering is not triggered if sudo access cannot be checked", %{
     handle_task_result: handle_task_result
   } do
     server = build_active_server(set_up_at: true, ssh_port: true)
@@ -1958,6 +1958,232 @@ defmodule ArchiDep.Servers.ServerTracking.ServerManagerStateTest do
                 problems: result.problems,
                 version: result.version + 1
               ), %ServerManagerState{result | version: result.version + 1}}
+  end
+
+  test "receive load average from the server", %{
+    handle_task_result: handle_task_result
+  } do
+    server = build_active_server(set_up_at: nil, ssh_port: true)
+
+    fake_get_load_average_ref = make_ref()
+
+    connected = ServersFactory.random_connected_state()
+
+    initial_state =
+      ServersFactory.build(:server_manager_state,
+        connection_state: connected,
+        server: server,
+        username: server.username,
+        tasks: %{get_load_average: fake_get_load_average_ref}
+      )
+
+    result =
+      handle_task_result.(
+        initial_state,
+        fake_get_load_average_ref,
+        {:ok, "0.65 0.43 0.21 1/436 761182\n", "", 0}
+      )
+
+    assert %{
+             actions:
+               [
+                 {:demonitor, ^fake_get_load_average_ref},
+                 {:send_message, send_message_fn}
+               ] = actions
+           } = result
+
+    assert result == %ServerManagerState{
+             initial_state
+             | actions: actions,
+               tasks: %{}
+           }
+
+    fake_timer_ref = make_ref()
+
+    assert send_message_fn.(result, fn :measure_load_average, 20_000 ->
+             fake_timer_ref
+           end) == %ServerManagerState{result | load_average_timer: fake_timer_ref}
+  end
+
+  test "receive malformed load average from the server", %{
+    handle_task_result: handle_task_result
+  } do
+    server = build_active_server(set_up_at: nil, ssh_port: true)
+
+    fake_get_load_average_ref = make_ref()
+
+    connected = ServersFactory.random_connected_state()
+
+    initial_state =
+      ServersFactory.build(:server_manager_state,
+        connection_state: connected,
+        server: server,
+        username: server.username,
+        tasks: %{get_load_average: fake_get_load_average_ref}
+      )
+
+    result =
+      handle_task_result.(
+        initial_state,
+        fake_get_load_average_ref,
+        {:ok, "oops", "", 0}
+      )
+
+    assert %{
+             actions:
+               [
+                 {:demonitor, ^fake_get_load_average_ref},
+                 {:send_message, send_message_fn}
+               ] = actions
+           } = result
+
+    assert result == %ServerManagerState{
+             initial_state
+             | actions: actions,
+               tasks: %{}
+           }
+
+    fake_timer_ref = make_ref()
+
+    assert send_message_fn.(result, fn :measure_load_average, 20_000 ->
+             fake_timer_ref
+           end) == %ServerManagerState{result | load_average_timer: fake_timer_ref}
+  end
+
+  test "receive failed load average from the server", %{
+    handle_task_result: handle_task_result
+  } do
+    server = build_active_server(set_up_at: nil, ssh_port: true)
+
+    fake_get_load_average_ref = make_ref()
+
+    connected = ServersFactory.random_connected_state()
+
+    initial_state =
+      ServersFactory.build(:server_manager_state,
+        connection_state: connected,
+        server: server,
+        username: server.username,
+        tasks: %{get_load_average: fake_get_load_average_ref}
+      )
+
+    result =
+      handle_task_result.(
+        initial_state,
+        fake_get_load_average_ref,
+        {:ok, "", "Oops\n", Faker.random_between(1, 255)}
+      )
+
+    assert %{
+             actions:
+               [
+                 {:demonitor, ^fake_get_load_average_ref},
+                 {:send_message, send_message_fn}
+               ] = actions
+           } = result
+
+    assert result == %ServerManagerState{
+             initial_state
+             | actions: actions,
+               tasks: %{}
+           }
+
+    fake_timer_ref = make_ref()
+
+    assert send_message_fn.(result, fn :measure_load_average, 20_000 ->
+             fake_timer_ref
+           end) == %ServerManagerState{result | load_average_timer: fake_timer_ref}
+  end
+
+  test "receive load average error from the server", %{
+    handle_task_result: handle_task_result
+  } do
+    server = build_active_server(set_up_at: nil, ssh_port: true)
+
+    fake_get_load_average_ref = make_ref()
+
+    connected = ServersFactory.random_connected_state()
+
+    initial_state =
+      ServersFactory.build(:server_manager_state,
+        connection_state: connected,
+        server: server,
+        username: server.username,
+        tasks: %{get_load_average: fake_get_load_average_ref}
+      )
+
+    result =
+      handle_task_result.(
+        initial_state,
+        fake_get_load_average_ref,
+        {:error, Faker.Lorem.sentence()}
+      )
+
+    assert %{
+             actions:
+               [
+                 {:demonitor, ^fake_get_load_average_ref},
+                 {:send_message, send_message_fn}
+               ] = actions
+           } = result
+
+    assert result == %ServerManagerState{
+             initial_state
+             | actions: actions,
+               tasks: %{}
+           }
+
+    fake_timer_ref = make_ref()
+
+    assert send_message_fn.(result, fn :measure_load_average, 20_000 ->
+             fake_timer_ref
+           end) == %ServerManagerState{result | load_average_timer: fake_timer_ref}
+  end
+
+  test "receive load average from the server while another task is in progress", %{
+    handle_task_result: handle_task_result
+  } do
+    server = build_active_server(set_up_at: nil, ssh_port: true)
+
+    fake_check_access_ref = make_ref()
+    fake_get_load_average_ref = make_ref()
+
+    connected = ServersFactory.random_connected_state()
+
+    initial_state =
+      ServersFactory.build(:server_manager_state,
+        connection_state: connected,
+        server: server,
+        username: server.username,
+        tasks: %{check_access: fake_check_access_ref, get_load_average: fake_get_load_average_ref}
+      )
+
+    result =
+      handle_task_result.(
+        initial_state,
+        fake_get_load_average_ref,
+        {:ok, "0.65 0.43 0.21 1/436 761182\n", "", 0}
+      )
+
+    assert %{
+             actions:
+               [
+                 {:demonitor, ^fake_get_load_average_ref},
+                 {:send_message, send_message_fn}
+               ] = actions
+           } = result
+
+    assert result == %ServerManagerState{
+             initial_state
+             | actions: actions,
+               tasks: %{check_access: fake_check_access_ref}
+           }
+
+    fake_timer_ref = make_ref()
+
+    assert send_message_fn.(result, fn :measure_load_average, 20_000 ->
+             fake_timer_ref
+           end) == %ServerManagerState{result | load_average_timer: fake_timer_ref}
   end
 
   defp assert_connect_fn(connect_fn, state, username, test_pid) do
