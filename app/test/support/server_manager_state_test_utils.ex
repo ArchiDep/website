9e57a56522f2b2417d6106c6e10d130f43c65634
Simon Oulevay <simon.oulevay@gmail.com>
2025-08-31 21:18:16 +0200

    Split server manager state monster test module

---
+++ app/test/support/server_manager_state_test_utils.ex
@@ -0,0 +1,202 @@
+defmodule ArchiDep.Support.ServerManagerStateTestUtils do
+  @moduledoc """
+  Utility functions for testing server manager state handling.
+  """
+
+  import ArchiDep.Helpers.PipeHelpers
+  import ArchiDep.Servers.ServerTracking.ServerConnectionState
+  import ExUnit.Assertions
+  alias ArchiDep.Accounts.Schemas.UserAccount
+  alias ArchiDep.Course.Schemas.User
+  alias ArchiDep.Repo
+  alias ArchiDep.Servers.Schemas.Server
+  alias ArchiDep.Servers.Schemas.ServerGroup
+  alias ArchiDep.Servers.Schemas.ServerGroupMember
+  alias ArchiDep.Servers.Schemas.ServerOwner
+  alias ArchiDep.Servers.Schemas.ServerRealTimeState
+  alias ArchiDep.Servers.ServerTracking.ServerManagerState
+  alias ArchiDep.Support.AccountsFactory
+  alias ArchiDep.Support.CourseFactory
+  alias ArchiDep.Support.FactoryHelpers
+  alias ArchiDep.Support.ServersFactory
+  alias Ecto.UUID
+
+  @type connect_fn :: (ServerManagerState.t(),
+                       (:inet.ip_address(), 1..65_535, String.t(), Keyword.t() -> Task.t()) ->
+                         ServerManagerState.t())
+
+  @spec assert_connect_fn!(connect_fn(), ServerManagerState.t(), String.t()) ::
+          ServerManagerState.t()
+  def assert_connect_fn!(connect_fn, state, username) do
+    fake_task = Task.completed(:fake)
+
+    expected_host = state.server.ip_address.address
+    expected_port = state.server.ssh_port || 22
+    expected_opts = [silently_accept_hosts: true]
+
+    result =
+      connect_fn.(state, fn ^expected_host, ^expected_port, ^username, ^expected_opts ->
+        fake_task
+      end)
+
+    assert result == %ServerManagerState{state | tasks: %{connect: fake_task.ref}}
+
+    result
+  end
+
+  @spec build_active_server(Keyword.t()) :: Server.t()
+  def build_active_server(opts! \\ []) do
+    {group, opts!} =
+      Keyword.pop_lazy(opts!, :group, fn ->
+        ServersFactory.build(:server_group, active: true, servers_enabled: true)
+      end)
+
+    {root, opts!} = Keyword.pop_lazy(opts!, :root, &FactoryHelpers.bool/0)
+
+    member =
+      if root do
+        nil
+      else
+        ServersFactory.build(:server_group_member, active: true, group: group)
+      end
+
+    owner = ServersFactory.build(:server_owner, active: true, root: root, group_member: member)
+
+    ServersFactory.build(
+      :server,
+      Keyword.merge([active: true, group: group, group_id: group.id, owner: owner], opts!)
+    )
+  end
+
+  @spec insert_active_server!(Keyword.t()) :: Server.t()
+  def insert_active_server!(opts!) do
+    class_id = UUID.generate()
+
+    {class_expected_server_properties, opts!} =
+      Keyword.pop(opts!, :class_expected_server_properties, [])
+
+    expected_server_properties =
+      CourseFactory.insert(
+        :expected_server_properties,
+        Keyword.merge(class_expected_server_properties, id: class_id)
+      )
+
+    class =
+      CourseFactory.insert(:class,
+        id: class_id,
+        active: true,
+        servers_enabled: true,
+        expected_server_properties: expected_server_properties
+      )
+
+    {:ok, group} = ServerGroup.fetch_server_group(class.id)
+
+    root = FactoryHelpers.bool()
+
+    member =
+      if root do
+        nil
+      else
+        user_account = AccountsFactory.insert(:user_account)
+        {:ok, user} = User.fetch_user(user_account.id)
+
+        student =
+          CourseFactory.insert(:student,
+            active: true,
+            class: class,
+            class_id: group.id,
+            user: user,
+            user_id: user.id
+          )
+
+        student.id |> ServerGroupMember.fetch_server_group_member() |> unpair_ok()
+      end
+
+    owner =
+      if member do
+        Repo.update_all(UserAccount, set: [preregistered_user_id: member.id])
+        member.owner_id |> ServerOwner.fetch_server_owner() |> unpair_ok()
+      else
+        user_account = AccountsFactory.insert(:user_account, active: true, root: true)
+        user_account.id |> ServerOwner.fetch_server_owner() |> unpair_ok()
+      end
+
+    id = UUID.generate()
+
+    {server_expected_properties, opts!} =
+      Keyword.pop(opts!, :server_expected_properties, [])
+
+    expected_properties =
+      ServersFactory.insert(:server_properties, Keyword.merge(server_expected_properties, id: id))
+
+    {server_last_known_properties, opts!} = Keyword.pop(opts!, :server_last_known_properties, [])
+
+    last_known_properties =
+      if server_last_known_properties == [] do
+        nil
+      else
+        ServersFactory.insert(
+          :server_properties,
+          Keyword.merge(server_last_known_properties, id: UUID.generate())
+        )
+      end
+
+    ServersFactory.insert(
+      :server,
+      Keyword.merge(
+        [
+          id: id,
+          active: true,
+          group: group,
+          group_id: group.id,
+          owner: owner,
+          owner_id: owner.id,
+          expected_properties: expected_properties,
+          last_known_properties: last_known_properties
+        ],
+        opts!
+      )
+    )
+
+    id |> Server.fetch_server() |> unpair_ok()
+  end
+
+  @spec real_time_state(Server.t(), Keyword.t()) :: ServerRealTimeState.t()
+  def real_time_state(server, attrs! \\ []) do
+    {connection_state, attrs!} =
+      Keyword.pop_lazy(attrs!, :connection_state, fn -> not_connected_state() end)
+
+    {conn_params, attrs!} = Keyword.pop_lazy(attrs!, :conn_params, fn -> conn_params(server) end)
+    {current_job, attrs!} = Keyword.pop(attrs!, :current_job, nil)
+    {problems, attrs!} = Keyword.pop(attrs!, :problems, [])
+    {set_up_at, attrs!} = Keyword.pop_lazy(attrs!, :set_up_at, fn -> server.set_up_at end)
+    {version, attrs!} = Keyword.pop(attrs!, :version, 0)
+
+    [] = Keyword.keys(attrs!)
+
+    %ServerRealTimeState{
+      connection_state: connection_state,
+      name: server.name,
+      conn_params: conn_params,
+      username: server.username,
+      app_username: server.app_username,
+      current_job: current_job,
+      problems: problems,
+      set_up_at: set_up_at,
+      version: version
+    }
+  end
+
+  @spec conn_params(Server.t(), Keyword.t()) :: {:inet.ip_address(), 1..65_535, String.t()}
+  def conn_params(server, attrs! \\ []) do
+    {username, attrs!} = Keyword.pop(attrs!, :username, server.username)
+
+    [] = Keyword.keys(attrs!)
+
+    {server.ip_address.address, server.ssh_port || 22, username}
+  end
+
+  @spec ssh_public_key() :: String.t()
+  def ssh_public_key,
+    do: :archidep |> Application.fetch_env!(:servers) |> Keyword.fetch!(:ssh_public_key)
+end
