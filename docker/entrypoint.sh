0e66a9aec92f116db7e8474d77b598b0d765078f
Simon Oulevay <simon.oulevay@gmail.com>
2025-09-04 21:54:47 +0200

    Do not fail on changing dir permissions in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,13 @@ if [ "$ARCHIDEP_UID" != 42000 ]; then
   echo "Changing archidep user and group to UID:GID ${ARCHIDEP_UID}:${ARCHIDEP_GID}..."
   usermod -u "$ARCHIDEP_UID" archidep 2>/dev/null
   groupmod -g "$ARCHIDEP_GID" archidep 2>/dev/null
-  chown "$ARCHIDEP_UID:$ARCHIDEP_GID" /archidep /home/archidep /etc/archidep /etc/archidep/ssh /var/lib/archidep /var/lib/archidep/uploads
+
+  set +e
+  for dir in /archidep /home/archidep /etc/archidep /etc/archidep/ssh /var/lib/archidep /var/lib/archidep/uploads; do
+    chown "$ARCHIDEP_UID:$ARCHIDEP_GID" "$dir"
+  done
+  set -e
+
   set -- gosu "${ARCHIDEP_UID}:${ARCHIDEP_GID}" "${@}"
   echo "Done changing UID:GID"
 fi
