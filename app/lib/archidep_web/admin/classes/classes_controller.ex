2caf5b795d04e53e77ea636adc81a220c756a1fa
Simon Oulevay <simon.oulevay@gmail.com>
2025-09-09 23:59:58 +0200

    Fix lint issue

--- app/lib/archidep_web/admin/classes/classes_controller.ex
+++ app/lib/archidep_web/admin/classes/classes_controller.ex
@@ -7,29 +7,30 @@ defmodule ArchiDepWeb.Admin.Classes.ClassesController do
   def generate_class_ssh_exercise_vm_inventory(conn, %{"id" => id}) do
     auth = conn.assigns.auth
 
-    with {:ok, class} <- Course.fetch_class(auth, id) do
-      students = Course.list_students(auth, class)
+    case Course.fetch_class(auth, id) do
+      {:ok, class} ->
+        students = Course.list_students(auth, class)
 
-      conn
-      |> put_resp_header("content-type", "application/json; charset=utf-8")
-      |> put_resp_header(
-        "content-disposition",
-        "attachment; filename=\"inventory.yml\""
-      )
-      |> send_resp(
-        200,
-        Jason.encode!(
-          %{
-            "students" =>
-              Enum.map(
-                students,
-                &%{"username" => &1.username, "password" => &1.ssh_exercise_password}
-              )
-          },
-          pretty: true
+        conn
+        |> put_resp_header("content-type", "application/json; charset=utf-8")
+        |> put_resp_header(
+          "content-disposition",
+          "attachment; filename=\"inventory.yml\""
         )
-      )
-    else
+        |> send_resp(
+          200,
+          Jason.encode!(
+            %{
+              "students" =>
+                Enum.map(
+                  students,
+                  &%{"username" => &1.username, "password" => &1.ssh_exercise_password}
+                )
+            },
+            pretty: true
+          )
+        )
+
       _anything ->
         send_resp(conn, 401, "Unauthorized")
     end
