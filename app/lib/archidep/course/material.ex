c1f434a7d71c30aa10fa4d16d25c769ee5372e80
Simon Oulevay <simon.oulevay@gmail.com>
2025-09-16 00:07:07 +0200

    Split course material data parsing into helper module

--- app/lib/archidep/course/material.ex
+++ app/lib/archidep/course/material.ex
@@ -4,45 +4,14 @@ defmodule ArchiDep.Course.Material do
   process.
   """
 
-  @course_material_dir Path.expand("../../../priv/static", __DIR__)
-  @course_material_file Path.join(@course_material_dir, "archidep.json")
-  @course_material_file_contents File.read!(@course_material_file)
-  @course_material_data JSON.decode!(@course_material_file_contents)
-  @course_material_file_digest :crypto.hash(
-                                 :sha256,
-                                 @course_material_file_contents
-                               )
+  alias ArchiDep.Course.Helpers.MaterialHelpers
 
-  %{"sections" => [_section | _rest] = sections} = @course_material_data
-
-  %{"title" => "Basic Deployment", "num" => 400, "docs" => docs} =
-    Enum.find(sections, &match?(%{"num" => 400}, &1))
-
-  %{
-    "num" => 402,
-    "title" => run_virtual_server_exercise_title,
-    "url" => run_virtual_server_exercise_url = "/course/402-run-virtual-server/"
-  } =
-    Enum.find(docs, &match?(%{"num" => 402}, &1))
-
-  @course_sections sections
-  @run_virtual_server_exercise_title run_virtual_server_exercise_title
-  @run_virtual_server_exercise_url run_virtual_server_exercise_url
+  @course_sections MaterialHelpers.course_sections()
+  @run_virtual_server_exercise MaterialHelpers.course_document(402, "run-virtual-server")
 
   @spec course_sections() :: list(map())
   def course_sections, do: @course_sections
 
   @spec run_virtual_server_exercise() :: %{title: String.t(), url: String.t()}
-  def run_virtual_server_exercise,
-    do: %{title: @run_virtual_server_exercise_title, url: @run_virtual_server_exercise_url}
-
-  @spec __mix_recompile__?() :: boolean()
-  def __mix_recompile__?, do: @course_material_file_digest != course_material_file_digest()
-
-  defp course_material_file_digest,
-    do:
-      :crypto.hash(
-        :sha256,
-        File.read!(@course_material_file)
-      )
+  def run_virtual_server_exercise, do: @run_virtual_server_exercise
 end
